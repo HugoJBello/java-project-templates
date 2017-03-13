7900d93f4c7f0d7b2f72ec7deb1fb8703f35c92c
Hugo J. Bello <hugo.bello.gu@gmail.com>
2017-03-13 20:57:04 +0100

    example of find added

--- jpa-mysql-example/src/main/App.java
+++ jpa-mysql-example/src/main/App.java
@@ -12,8 +12,8 @@ public class App {
 	// to insert data into the database:  
 	User exampleUser = new User();
 	UserPK pk = new UserPK();
-	pk.setUsername("username");
-	pk.setPassword("pw");
+	pk.setUsername("username2");
+	pk.setPassword("pw2");
 	
 	exampleUser.setIdPk(pk);
 	exampleUser.setName("Joe");
@@ -25,6 +25,17 @@ public class App {
     em.persist(exampleUser);
     em.getTransaction()
         .commit();
+    
+    UserPK pkToFind = new UserPK();
+    pkToFind.setUsername("hjbello");
+    pkToFind.setPassword("1234");   
+    try {
+		User foundUser = em.find(User.class, pkToFind);
+		System.out.println(foundUser.getEmail());
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+    
     em.close();
     PersistenceManager.INSTANCE.close();
   }
