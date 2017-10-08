403cf352b3320210274399e359ab10ae7daa6d32
HugoJBello <hugo.bello.gu@gmail.com>
2017-10-08 21:35:12 +0200

    -

--- cucumber-wiki-spring/src/main/resources/set_up_database.sql
+++ cucumber-wiki-spring/src/main/resources/set_up_database.sql
@@ -53,13 +53,16 @@ CREATE TABLE page_entries (
   CONSTRAINT fk_created_by_cathegories FOREIGN KEY (created_by) REFERENCES users (userid));
 
 CREATE TABLE cathegories_referenced (
+  reference_id int(11) NOT NULL AUTO_INCREMENT,
   cathegory_name VARCHAR(500) NOT NULL,
+  cathegory_title text,
   entry_name_referenced VARCHAR(500) NOT NULL,
-  referenced_at datetime,
+  updated_at datetime,
   KEY fk_cathegory_name (cathegory_name),
   KEY fk_entry_name_referenced (entry_name_referenced),
   CONSTRAINT fk_cathegory_name FOREIGN KEY (cathegory_name) REFERENCES cathegories (cathegory_name),
-  CONSTRAINT fk_entry_name_referenced FOREIGN KEY (entry_name_referenced) REFERENCES page_entries (entry_name));
+  CONSTRAINT fk_entry_name_referenced FOREIGN KEY (entry_name_referenced) REFERENCES page_entries (entry_name),
+  PRIMARY KEY (reference_id));
 
 
 
@@ -73,70 +76,49 @@ VALUES (001, 'ROLE_USER');
 INSERT INTO user_roles (userid, role)
 VALUES (001, 'ROLE_ADMIN');
 
-INSERT INTO page_entries( title, entry_name, contents,cathegories,created_by,updated_by)
-values ('entry test 1', 'entry_test_1', '## Title \n **bold** this is entry test 1\n *italic*', 'cathegory 1; cathegory 2',001,001);
+INSERT INTO page_entries( title, entry_name, contents,cathegories,created_by,updated_by,updated_at)
+values ('entry test 1', 'entry_test_1', '## Title \n **bold** this is entry test 1\n *italic*', 'cathegory 1; cathegory 2',001,001,sysdate());
 
 
-INSERT INTO page_entries( title, entry_name, contents,cathegories,created_by,updated_by)
-values ('entry test 2', 'entry_test_2', '## Title \n **bold** this is entry test 1\n *italic*', 'cathegory 1; cathegory 2',001,001);
-INSERT INTO page_entries( title, entry_name, contents,cathegories,created_by,updated_by)
-values ('entry test 3', 'entry_test_3', '## Title \n **bold** this is entry test 1\n *italic*', 'cathegory 1; cathegory 2',001,001);
-INSERT INTO page_entries( title, entry_name, contents,cathegories,created_by,updated_by)
-values ('entry test 4', 'entry_test_4', '## Title \n **bold** this is entry test 1\n *italic*', 'cathegory 1; cathegory 2',001,001);
-INSERT INTO page_entries( title, entry_name, contents,cathegories,created_by,updated_by)
-values ('entry test 5', 'entry_test_5', '## Title \n **bold** this is entry test 1\n *italic*', 'cathegory 1; cathegory 2',001,001);
-INSERT INTO page_entries( title, entry_name, contents,cathegories,created_by,updated_by)
-values ('entry test 6', 'entry_test_6', '## Title \n **bold** this is entry test 1\n *italic*', 'cathegory 1; cathegory 2',001,001);
-INSERT INTO page_entries( title, entry_name, contents,cathegories,created_by,updated_by)
-values ('entry test 7', 'entry_test_7', '## Title \n **bold** this is entry test 1\n *italic*', 'cathegory test; cathegory 2',001,001);
+INSERT INTO page_entries( title, entry_name, contents,cathegories,created_by,updated_by,updated_at)
+values ('entry test 2', 'entry_test_2', '## Title \n **bold** this is entry test 1\n *italic*', 'cathegory 1; cathegory 2',001,001,sysdate());
+INSERT INTO page_entries( title, entry_name, contents,cathegories,created_by,updated_by,updated_at)
+values ('entry test 3', 'entry_test_3', '## Title \n **bold** this is entry test 1\n *italic*', 'cathegory 1; cathegory 2',001,001,sysdate());
+INSERT INTO page_entries( title, entry_name, contents,cathegories,created_by,updated_by,updated_at)
+values ('entry test 4', 'entry_test_4', '## Title \n **bold** this is entry test 1\n *italic*', 'cathegory 1; cathegory 2',001,001,sysdate());
+INSERT INTO page_entries( title, entry_name, contents,cathegories,created_by,updated_by,updated_at)
+values ('entry test 5', 'entry_test_5', '## Title \n **bold** this is entry test 1\n *italic*', 'cathegory 1; cathegory 2',001,001,sysdate());
+INSERT INTO page_entries( title, entry_name, contents,cathegories,created_by,updated_by,updated_at)
+values ('entry test 6', 'entry_test_6', '## Title \n **bold** this is entry test 1\n *italic*', 'cathegory 1; cathegory 2',001,001,sysdate());
+INSERT INTO page_entries( title, entry_name, contents,cathegories,created_by,updated_by,updated_at)
+values ('entry test 7', 'entry_test_7', '## Title \n **bold** this is entry test 1\n *italic*', 'cathegory test; cathegory 2',001,001,sysdate());
 
 
-INSERT INTO page_entries( title, entry_name, contents,cathegories,created_by,updated_by)
-values ('entry test 8', 'entry_test_8', '## Title \n **bold** this is entry test 1\n *italic*', 'cathegory 1; cathegory 2',001,001);
-INSERT INTO page_entries( title, entry_name, contents,cathegories,created_by,updated_by)
-values ('entry test 9', 'entry_test_9', '## Title \n **bold** this is entry test 1\n *italic*', 'cathegory 1; cathegory 2',001,001);
-INSERT INTO page_entries( title, entry_name, contents,cathegories,created_by,updated_by)
-values ('entry test 10', 'entry_test_10', '## Title \n **bold** this is entry test 1\n *italic*', 'cathegory 1; cathegory 2',001,001);
-INSERT INTO page_entries( title, entry_name, contents,cathegories,created_by,updated_by)
-values ('entry test 11', 'entry_test_11', '## Title \n **bold** this is entry test 1\n *italic*', 'cathegory 1; cathegory 2',001,001);
-INSERT INTO page_entries( title, entry_name, contents,cathegories,created_by,updated_by)
-values ('entry test 12', 'entry_test_12', '## Title \n **bold** this is entry test 1\n *italic*', 'cathegory 1; cathegory 2',001,001);
-INSERT INTO page_entries( title, entry_name, contents,cathegories,created_by,updated_by)
-values ('entry test 13', 'entry_test_13', '## Title \n **bold** this is entry test 1\n *italic*', 'cathegory test; cathegory 2',001,001);
+INSERT INTO page_entries( title, entry_name, contents,cathegories,created_by,updated_by,updated_at)
+values ('entry test 8', 'entry_test_8', '## Title \n **bold** this is entry test 1\n *italic*', 'cathegory 1; cathegory 2',001,001,sysdate());
+INSERT INTO page_entries( title, entry_name, contents,cathegories,created_by,updated_by,updated_at)
+values ('entry test 9', 'entry_test_9', '## Title \n **bold** this is entry test 1\n *italic*', 'cathegory 1; cathegory 2',001,001,sysdate());
+INSERT INTO page_entries( title, entry_name, contents,cathegories,created_by,updated_by,updated_at)
+values ('entry test 10', 'entry_test_10', '## Title \n **bold** this is entry test 1\n *italic*', 'cathegory 1; cathegory 2',001,001,sysdate());
+INSERT INTO page_entries( title, entry_name, contents,cathegories,created_by,updated_by,updated_at)
+values ('entry test 11', 'entry_test_11', '## Title \n **bold** this is entry test 1\n *italic*', 'cathegory 1; cathegory 2',001,001,sysdate());
+INSERT INTO page_entries( title, entry_name, contents,cathegories,created_by,updated_by,updated_at)
+values ('entry test 12', 'entry_test_12', '## Title \n **bold** this is entry test 1\n *italic*', 'cathegory 1; cathegory 2',001,001,sysdate());
+INSERT INTO page_entries( title, entry_name, contents,cathegories,created_by,updated_by,updated_at)
+values ('entry test 13', 'entry_test_13', '## Title \n **bold** this is entry test 1\n *italic*', 'cathegory test; cathegory 2',001,001,sysdate());
 
 
-INSERT INTO cathegories(title, cathegory_name, created_by,updated_by)
-values ('cathegory test','cathegory_test', 001,001);
+INSERT INTO cathegories(title, cathegory_name, created_by,updated_by,updated_at)
+values ('cathegory test','cathegory_test', 001,001,sysdate());
 
-INSERT INTO cathegories(title, cathegory_name, created_by,updated_by)
-values ('cathegory 1','cathegory_1', 001,001);
+INSERT INTO cathegories(title, cathegory_name, created_by,updated_by,updated_at)
+values ('cathegory 1','cathegory_1', 001,001,sysdate());
 
-insert into cathegories_referenced(cathegory_name,entry_name_referenced)
-values ('cathegory_1', 'entry_test_1');
-insert into cathegories_referenced(cathegory_name,entry_name_referenced)
-values ('cathegory_1', 'entry_test_2');
-insert into cathegories_referenced(cathegory_name,entry_name_referenced)
-values ('cathegory_1', 'entry_test_3');
-insert into cathegories_referenced(cathegory_name,entry_name_referenced)
-values ('cathegory_1', 'entry_test_4');
-insert into cathegories_referenced(cathegory_name,entry_name_referenced)
-values ('cathegory_1', 'entry_test_5');
-insert into cathegories_referenced(cathegory_name,entry_name_referenced)
-values ('cathegory_1', 'entry_test_6');
-insert into cathegories_referenced(cathegory_name,entry_name_referenced)
-values ('cathegory_1', 'entry_test_7');
-insert into cathegories_referenced(cathegory_name,entry_name_referenced)
-values ('cathegory_1', 'entry_test_8');
-insert into cathegories_referenced(cathegory_name,entry_name_referenced)
-values ('cathegory_1', 'entry_test_9');
-insert into cathegories_referenced(cathegory_name,entry_name_referenced)
-values ('cathegory_1', 'entry_test_10');
-insert into cathegories_referenced(cathegory_name,entry_name_referenced)
-values ('cathegory_1', 'entry_test_11');
-insert into cathegories_referenced(cathegory_name,entry_name_referenced)
-values ('cathegory_1', 'entry_test_12');
-insert into cathegories_referenced(cathegory_name,entry_name_referenced)
-values ('cathegory_1', 'entry_test_13');
+insert into cathegories_referenced(cathegory_name,entry_name_referenced,updated_at)
+values ('cathegory_2', 'entry_test_2',sysdate());
+
+
+
 
 
 select * from users;
